c418466954f801e8095cf9cfb17c1b294d26457d
Martin Lalutov <47317818+Lalutoww@users.noreply.github.com>
2026-07-20 18:42:38 +0300

    recurring problems and patterns solutions

--- part02-Part02_02.SquareRootOfSum/src/main/java/SquareRootOfSum.java
+++ part02-Part02_02.SquareRootOfSum/src/main/java/SquareRootOfSum.java
@@ -5,6 +5,9 @@ public class SquareRootOfSum {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
+            
+        int num1 = scanner.nextInt();
+        int num2 = scanner.nextInt();
+        System.out.println(Math.sqrt(num1+num2));
     }
 }
--- part02-Part02_03.AbsoluteValue/src/main/java/AbsoluteValue.java
+++ part02-Part02_03.AbsoluteValue/src/main/java/AbsoluteValue.java
@@ -6,5 +6,11 @@ public class AbsoluteValue {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        int number = scanner.nextInt();
+        if (number < 0) {
+            number *= -1;
+        }
+
+        System.out.println(number);
     }
 }
